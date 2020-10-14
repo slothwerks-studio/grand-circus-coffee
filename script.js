064fc26f501f77661548c58912eae0768ebd76e9
Sloth I <github@slothwerks.com>
2020-10-14 17:25:49 -0400

    Added JavaScript updates per student guide

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 // add variables here
+let price;
+let drink;
 
 function checkout(choice) {
   document.querySelector("#checkout").classList.remove("hidden");
   document.querySelector("#checkout-img").src = `${choice}.jpg`; //look for other concatenation
 
   // add the conditional below
+  if (choice === "latte") {
+    price = 4.29;
+    drink = "Latte";
+  } else if (choice === "cappuccino") {
+    price = 4.79;
+    drink = "Cappuccino";
+  } else if (choice === "iced-coffee") {
+    price = 3.79;
+    drink = "Iced Coffee";
+  }
 
   document.querySelector("#selected-drink").innerText = drink;
   document.querySelector("#price").innerText = "$" + price;
@@ -14,9 +26,10 @@ function receipt() {
   document.querySelector("#checkout").classList.add("hidden");
   document.querySelector("#receipt").classList.remove("hidden");
 
-  let quantity = document.querySelector("#quantity1").value;
+  let quantity = parseInt(document.querySelector("#quantity1").value);
 
-  // add quantity and total calculation below!
+  // add total calculation below!
+  let total = quantity * price;
 
   document.querySelector("#drink").innerText = drink;
   document.querySelector("#total").innerText = "$" + total.toFixed(2);
