5e81df8d17d92e09e92eb258fb449b6d439badf2
Sloth I <github@slothwerks.com>
2020-10-14 20:32:35 -0400

    Updated cart object and user flow when selecting a drinkk; still need to update receipt

--- script.js
+++ script.js
@@ -1,44 +1,55 @@
-// add variables here
-let price;
-let drink;
+// Add variables here
+// These are used when the user is updating the cart
+// You can declare a number of variables on the same line
+// without initializing
+let price, drink, currentChoice;
 
-// build cart object
+// Build cart object
+// This is sort of like a menu where we describe the items available
+// and the quantity that are being purchased by the customer
 let cart = {
   latte: {
+    image: "latte.jpg",
     price: 4.29,
     label: "Latte",
     quantity: 0
   },
   cappuccino: {
+    image: "cappuccino.jpg",
     price: 4.79,
     label: "Cappuccino",
     quantity: 0
   },
   icedCoffee: {
+    image: "iced-coffee.jpg",
     price: 3.79,
     label: "Iced Coffee",
     quantity: 0
   }
 };
 
+// Build a function that allows the customer to update the quantity
+// of an item in the cart
 function checkout(choice) {
   document.querySelector("#checkout").classList.remove("hidden");
-  document.querySelector("#checkout-img").src = `${choice}.jpg`; //look for other concatenation
+  document.querySelector("#checkout-img").src = cart[choice].image;
 
-  // add the conditional below
-  if (choice === "latte") {
-    price = 4.29;
-    drink = "Latte";
-  } else if (choice === "cappuccino") {
-    price = 4.79;
-    drink = "Cappuccino";
-  } else if (choice === "iced-coffee") {
-    price = 3.79;
-    drink = "Iced Coffee";
-  }
+  // Update variables based on choice
+  currentChoice = choice;
+  price = cart[choice].price;
+  drink = cart[choice].label;
+  quantity = cart[choice].quantity;
 
   document.querySelector("#selected-drink").innerText = drink;
   document.querySelector("#price").innerText = "$" + price;
+  document.querySelector("#quantity1").value = quantity; // Need to inject the current quantity into the DOM
+}
+
+function updateCart() {
+  document.querySelector("#checkout").classList.add("hidden");
+  let quantity = parseInt(document.querySelector("#quantity1").value);
+  cart[currentChoice].quantity = quantity;
+  console.log(cart);
 }
 
 function receipt() {
